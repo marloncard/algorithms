1b2ca04722b1ddf8e677c2f1698efd86ffa9ac40
Marlon Card <marloncard@gmail.com>
2019-05-24 09:19:57 -0400

    Add selection sort

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A collection of typical and atypical algorithms
 
 I will add to this collection as often as possible. At the moment these will be in Python but I will translate to JavaScript as well.
+
+
+
+## Credits
+I'm currently working through the book "Fundamentals of Python: Data Structions" by Kenneth A. Lambert
---
+++ selection_sort.py
@@ -0,0 +1,28 @@
+"""
+The 'Selection Sort' algorithm searches the entire list for the position of the smallest item.
+If that position does not equal the first position, then those two items are swapped. The algorithm
+then returns to the second position and repeats this process, swapping the smallest item with the 
+item at the second position if necessary.
+"""
+
+
+def selection_sort(lyst):
+    i = 0
+    # Do n - 1 searches
+    while i < len(lyst) - 1:
+        min_index = i
+        j = i + 1
+        # Start a search
+        while j < len(lyst):
+            if lyst[j] < lyst[min_index]:
+                min_index = j
+            j += 1
+        # Exchange if necessary
+        if min_index != i:
+            lyst[min_index], lyst[i] = lyst[i], lyst[min_index]
+        i += 1
+    return lyst
+
+
+if __name__ == '__main__':
+    print(selection_sort([5,10,9,1,3,2,8,7]))
